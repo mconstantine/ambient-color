e3bbee204124680be2b8c49815335559af185d7f
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2026-04-04 20:03:28 +0200

    Made nvim more colorful.

--- templates/colors.lua
+++ templates/colors.lua
@@ -1,25 +1,25 @@
 -- Reference: https://daisyui.com/theme-generator
-local bg_800 = "{{ primary.w800.bg }}"
-local bg_900 = "{{ primary.w900.bg }}"
+local bg_800 = "{{ primary.w700.bg }}"
+local bg_900 = "{{ primary.w800.bg }}"
 local primary = "{{ opposite.w400.bg }}"
 local success = "#01df72" -- green 400
 local success_content = "#022d14" -- green 950
 
 local colors = {
-	bg_dim = "{{ neutral.w950.bg }}",
-	bg = "{{ primary.w950.bg }}",
-	bg_950 = "{{ primary.w950.bg }}",
+	bg_dim = "{{ neutral.w900.bg }}",
+	bg = "{{ primary.w900.bg }}",
+	bg_950 = "{{ primary.w900.bg }}",
 	bg_900 = bg_900,
 	bg_800 = bg_800,
 	bg_500 = "{{ primary.w500.bg }}",
-	fg = "{{ primary.w950.fg }}",
-	fg_dim = "{{ neutral.w950.fg }}",
+	fg = "{{ primary.w900.fg }}",
+	fg_dim = "{{ neutral.w900.fg }}",
 	primary = primary,
 	primary_content = "{{ opposite.w400.fg }}",
 	secondary_50 = "{{ secondary.w50.bg }}",
 	secondary = "{{ secondary.w400.bg }}",
 	secondary_800 = "{{ secondary.w800.bg }}",
-	secondary_content = "{{ secondary.w950.fg }}",
+	secondary_content = "{{ secondary.w900.fg }}",
 	success = success,
 	success_500 = "#00c850",
 	success_800 = "#00642e",
