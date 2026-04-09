8995a3bf1871c729ee6ecef3c364642f6607e9a1
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2026-04-09 15:16:12 +0200

    theme.sh now listens for changes.

--- theme.sh
+++ theme.sh
@@ -1,14 +1,26 @@
-if pgrep -x "Hyprland" > /dev/null; then
-  data=$(cat ~/.cache/ambient-color/data.json)
-  time=$(echo "$data" | jq -r '.time')
+#!/bin/bash
 
-  if [[ "$time" == "Sunrise" ]]; then
-    dark
-  elif [[ "$time" == "Day" ]]; then
-    light
-  elif [[ "$time" == "Sunset" ]]; then
-    dark
-  elif [[ "$time" == "Night" ]]; then
-    night
+update_theme() {
+  if pgrep -x "Hyprland" > /dev/null; then
+    data=$(cat ~/.cache/ambient-color/data.json)
+    time=$(echo "$data" | jq -r '.time')
+
+    if [[ "$time" == "Sunrise" ]]; then
+      dark
+    elif [[ "$time" == "Day" ]]; then
+      light
+    elif [[ "$time" == "Sunset" ]]; then
+      dark
+    elif [[ "$time" == "Night" ]]; then
+      night
+    fi
   fi
-fi
+}
+
+update_theme
+
+inotifywait -m -q -e close_write,moved_to --format "%f" ~/.cache/ambient-color/ | while read -r filename; do
+  if [[ "$filename" == "data.json" ]]; then
+    update_theme
+  fi
+done
