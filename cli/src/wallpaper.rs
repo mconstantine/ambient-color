62e9ee4a545ee4ffc53556e2c9e07d77c707f7ec
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2026-04-12 15:08:16 +0200

    Drawing wallpaper per monitor.

--- cli/src/wallpaper.rs
+++ cli/src/wallpaper.rs
@@ -1,17 +1,21 @@
-use std::process::{Command, Stdio};
+use std::{
+    f32::consts::PI,
+    process::{Command, Stdio},
+};
 
 use core_logic::{
     data::Theme,
     palette::{IntoColor, Srgb},
 };
 use serde::Deserialize;
-use tiny_skia::{Color, Paint, PathBuilder, Pixmap, Transform};
+use tiny_skia::{Color, LineCap, Paint, PathBuilder, Pixmap, Stroke, Transform};
 
 #[derive(Deserialize, Debug)]
 struct Monitor {
     name: String,
     width: u16,
     height: u16,
+    scale: f32,
 }
 
 pub fn draw_wallpaper(theme: &Theme) -> () {
@@ -29,8 +33,8 @@ pub fn draw_wallpaper(theme: &Theme) -> () {
     let monitors: Vec<Monitor> = serde_json::from_str(&monitors_string)
         .expect("Error: unable to parse monitors output to JSON");
 
-    for monitor in monitors {
-        draw_monitor_wallpaper(&monitor, theme);
+    for (index, monitor) in monitors.iter().enumerate() {
+        draw_monitor_wallpaper(&monitor, theme, index);
     }
 
     Command::new("hyprctl")
@@ -39,7 +43,7 @@ pub fn draw_wallpaper(theme: &Theme) -> () {
         .expect("Error: failed to unload old wallpapers");
 }
 
-fn draw_monitor_wallpaper(monitor: &Monitor, theme: &Theme) {
+fn draw_monitor_wallpaper(monitor: &Monitor, theme: &Theme, index: usize) {
     let width = monitor.width as u32;
     let height = monitor.height as u32;
     let path = format!("/tmp/ambient_color_wallpaper_{}.png", monitor.name);
@@ -50,8 +54,8 @@ fn draw_monitor_wallpaper(monitor: &Monitor, theme: &Theme) {
     let bg_srgb_u8: Srgb<u8> = bg_srgb_f32.into_format();
     let circle_srgb_f32: Srgb<f32> = theme.original_color.bg.into_color();
     let circle_srgb_u8: Srgb<u8> = circle_srgb_f32.into_format();
-    // let tree_srgb_f32: Srgb<f32> = theme.original_color.fg.into_color();
-    // let tree_srgb_u8: Srgb<u8> = tree_srgb_f32.into_format();
+    let tree_srgb_f32: Srgb<f32> = theme.original_color.fg.into_color();
+    let tree_srgb_u8: Srgb<u8> = tree_srgb_f32.into_format();
 
     let bg_color = Color::from_rgba8(bg_srgb_u8.red, bg_srgb_u8.green, bg_srgb_u8.blue, 255);
 
@@ -62,8 +66,8 @@ fn draw_monitor_wallpaper(monitor: &Monitor, theme: &Theme) {
         255,
     );
 
-    // let tree_color =
-    //     Color::from_rgba8(tree_srgb_u8.red, tree_srgb_u8.green, tree_srgb_u8.blue, 255);
+    let tree_color =
+        Color::from_rgba8(tree_srgb_u8.red, tree_srgb_u8.green, tree_srgb_u8.blue, 255);
 
     pixmap.fill(bg_color);
 
@@ -74,7 +78,7 @@ fn draw_monitor_wallpaper(monitor: &Monitor, theme: &Theme) {
 
     let center_x = width as f32 / 2.0;
     let center_y = height as f32 / 2.0;
-    let radius = 256.0;
+    let radius = 128.0 * monitor.scale;
     let mut path_builder = PathBuilder::new();
 
     path_builder.push_circle(center_x, center_y, radius);
@@ -89,56 +93,39 @@ fn draw_monitor_wallpaper(monitor: &Monitor, theme: &Theme) {
         None,
     );
 
-    // paint.set_color(tree_color);
-    //
-    // let mut stroke = Stroke::default();
-    //
-    // stroke.width = 8.0;
-    // stroke.line_cap = LineCap::Round;
-    //
-    // let hex_radius = radius / 2.0;
-    // let mut path_builder = PathBuilder::new();
-    //
-    // path_builder.move_to(
-    //     center_x + hex_radius * (PI / 3.0 * 0.0).sin(),
-    //     center_y + hex_radius * (PI / 3.0 * 0.0).cos(),
-    // );
-    //
-    // path_builder.line_to(
-    //     center_x + hex_radius * (PI / 3.0 * 1.0 as f32).sin(),
-    //     center_y + hex_radius * (PI / 3.0 * 1.0 as f32).cos(),
-    // );
-    //
-    // path_builder.line_to(
-    //     center_x + hex_radius * (PI / 3.0 * 2.0 as f32).sin(),
-    //     center_y + hex_radius * (PI / 3.0 * 2.0 as f32).cos(),
-    // );
-    //
-    // path_builder.line_to(
-    //     center_x + hex_radius * (PI / 3.0 * 3.0 as f32).sin(),
-    //     center_y + hex_radius * (PI / 3.0 * 3.0 as f32).cos(),
-    // );
-    //
-    // path_builder.line_to(
-    //     center_x + hex_radius * (PI / 3.0 * 4.0 as f32).sin(),
-    //     center_y + hex_radius * (PI / 3.0 * 4.0 as f32).cos(),
-    // );
-    //
-    // path_builder.line_to(
-    //     center_x + hex_radius * (PI / 3.0 * 5.0 as f32).sin(),
-    //     center_y + hex_radius * (PI / 3.0 * 5.0 as f32).cos(),
-    // );
-    //
-    // path_builder.line_to(
-    //     center_x + hex_radius * (PI / 3.0 * 6.0 as f32).sin(),
-    //     center_y + hex_radius * (PI / 3.0 * 6.0 as f32).cos(),
-    // );
-    //
-    // let hex_path = path_builder
-    //     .finish()
-    //     .expect("Error: unable to draw hexagon");
-    //
-    // pixmap.stroke_path(&hex_path, &paint, &stroke, Transform::identity(), None);
+    paint.set_color(tree_color);
+
+    let mut stroke = Stroke::default();
+
+    stroke.width = 4.0 * monitor.scale;
+    stroke.line_cap = LineCap::Round;
+
+    let hex_radius = radius / 2.0;
+    let mut path_builder = PathBuilder::new();
+    let sides_count = index + 4;
+
+    path_builder.move_to(
+        center_x + hex_radius * (2 as f32 * PI / sides_count as f32 * 0.0).sin(),
+        center_y + hex_radius * (2 as f32 * PI / sides_count as f32 * 0.0).cos(),
+    );
+
+    for i in 1..sides_count {
+        path_builder.line_to(
+            center_x + hex_radius * (2 as f32 * PI / sides_count as f32 * i as f32).sin(),
+            center_y + hex_radius * (2 as f32 * PI / sides_count as f32 * i as f32).cos(),
+        );
+    }
+
+    path_builder.line_to(
+        center_x + hex_radius * (2 as f32 * PI / sides_count as f32 * 0.0).sin(),
+        center_y + hex_radius * (2 as f32 * PI / sides_count as f32 * 0.0).cos(),
+    );
+
+    let polygon_path = path_builder
+        .finish()
+        .expect("Error: unable to draw polygon");
+
+    pixmap.stroke_path(&polygon_path, &paint, &stroke, Transform::identity(), None);
 
     pixmap
         .save_png(&path)
@@ -149,12 +136,12 @@ fn draw_monitor_wallpaper(monitor: &Monitor, theme: &Theme) {
         .output()
         .expect("Error: unable to preload wallpaper");
 
+    println!("hyprctl hyprpaper wallpaper {},{}", monitor.name, path);
+
+    let apply_arg = format!("{},{}", monitor.name, path);
+
     Command::new("hyprctl")
-        .args([
-            "hyprpaper",
-            "wallpaper",
-            &format!("{},{}", monitor.name, path),
-        ])
+        .args(["hyprpaper", "wallpaper", &apply_arg])
         .output()
         .expect("Error: failed to set wallpaper");
 }
