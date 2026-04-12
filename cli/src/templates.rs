600c705cbafedefc3efef606d2249c592d95ceb9
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2026-04-12 13:39:03 +0200

    Working on wallpaper.

---
+++ cli/src/templates.rs
@@ -0,0 +1,86 @@
+use std::{fs, path::Path};
+
+use core_logic::data::Theme;
+use directories::ProjectDirs;
+use minijinja::{Environment, Value, context};
+
+pub fn compile_config_files(theme: &Theme) -> () {
+    let mut env = Environment::new();
+
+    env.add_filter("no_hashtag", no_hashtag);
+
+    let context = context! {
+        original => theme.original_color,
+        primary => theme.primary_palette,
+        opposite => theme.opposite_palette,
+        secondary => theme.secondary_palette,
+        tertiary => theme.tertiary_palette,
+        neutral => theme.neutral_palette,
+    };
+
+    if let Some(project_dirs) = ProjectDirs::from("it", "mconst", "ambient-color") {
+        let config_dir = project_dirs.config_dir();
+        let cache_dir = project_dirs.cache_dir();
+        let templates_dir = config_dir.join("templates");
+
+        if let Err(error) = fs::create_dir_all(cache_dir) {
+            eprintln!("Failed to create cache directory: {}", error);
+            return;
+        }
+
+        let dirs: Directories = Directories {
+            source: &templates_dir,
+            destination: cache_dir,
+        };
+
+        digest_template(&dirs, &context, &mut env, "ashell", "ashell.toml");
+        digest_template(&dirs, &context, &mut env, "clipse", "clipse_theme.json");
+        digest_template(&dirs, &context, &mut env, "nvim", "colors.lua");
+        digest_template(&dirs, &context, &mut env, "fnott", "fnott.ini");
+        digest_template(&dirs, &context, &mut env, "foot", "foot.ini");
+        digest_template(&dirs, &context, &mut env, "gtk", "gtk.css");
+        digest_template(&dirs, &context, &mut env, "hyprland", "hyprland.conf");
+        digest_template(&dirs, &context, &mut env, "hyprlock", "hyprlock.conf");
+        digest_template(&dirs, &context, &mut env, "rofi", "rofi.rasi");
+        digest_template(&dirs, &context, &mut env, "tmux", "tmux.conf");
+        digest_template(&dirs, &context, &mut env, "zsh", ".zshrc");
+
+        match serde_json::to_string(theme) {
+            Ok(json) => {
+                if let Err(error) = fs::write(cache_dir.join("data.json"), json) {
+                    eprintln!("Unable to write data into cache: {}", error);
+                }
+            }
+            Err(error) => eprintln!("Failed to serialize data: {}", error),
+        }
+    } else {
+        eprintln!("Unable to access project directories");
+    }
+}
+
+struct Directories<'a> {
+    source: &'a Path,
+    destination: &'a Path,
+}
+
+fn digest_template<'a>(
+    directories: &Directories<'a>,
+    context: &Value,
+    env: &mut Environment,
+    template_name: &str,
+    file_path: &str,
+) -> () {
+    let template = fs::read_to_string(directories.source.join(file_path))
+        .expect(format!("{} template not found", template_name).as_str());
+
+    let rendered = env
+        .render_str(&template, context)
+        .expect(format!("Unable to render {} template", template_name).as_str());
+
+    fs::write(directories.destination.join(file_path), rendered)
+        .expect(format!("Unable to save rendered {} template", template_name).as_str());
+}
+
+fn no_hashtag(string: String) -> String {
+    String::from(string.strip_prefix("#").unwrap())
+}
---
+++ cli/src/wallpaper.rs
@@ -0,0 +1,130 @@
+use std::{cmp::min, process::Command};
+
+use core_logic::{
+    data::Theme,
+    palette::{IntoColor, Srgb},
+};
+use tiny_skia::{Color, Paint, PathBuilder, Pixmap, Transform};
+
+pub fn draw_wallpaper(theme: &Theme) -> () {
+    let width = 2560;
+    let height = 1600;
+    let path = "/tmp/ambient_color_wallpaper.png";
+
+    let mut pixmap = Pixmap::new(width, height).expect("Error: unable to create canvas");
+
+    let bg_srgb_f32: Srgb<f32> = theme.primary_palette.w950.bg.into_color();
+    let bg_srgb_u8: Srgb<u8> = bg_srgb_f32.into_format();
+    let circle_srgb_f32: Srgb<f32> = theme.original_color.bg.into_color();
+    let circle_srgb_u8: Srgb<u8> = circle_srgb_f32.into_format();
+    // let tree_srgb_f32: Srgb<f32> = theme.original_color.fg.into_color();
+    // let tree_srgb_u8: Srgb<u8> = tree_srgb_f32.into_format();
+
+    let bg_color = Color::from_rgba8(bg_srgb_u8.red, bg_srgb_u8.green, bg_srgb_u8.blue, 255);
+
+    let circle_color = Color::from_rgba8(
+        circle_srgb_u8.red,
+        circle_srgb_u8.green,
+        circle_srgb_u8.blue,
+        255,
+    );
+
+    // let tree_color =
+    //     Color::from_rgba8(tree_srgb_u8.red, tree_srgb_u8.green, tree_srgb_u8.blue, 255);
+
+    pixmap.fill(bg_color);
+
+    let mut paint = Paint::default();
+
+    paint.set_color(circle_color);
+    paint.anti_alias = true;
+
+    let center_x = width as f32 / 2.0;
+    let center_y = height as f32 / 2.0;
+    let radius = min(width, height) as f32 * 0.2;
+    let mut path_builder = PathBuilder::new();
+
+    path_builder.push_circle(center_x, center_y, radius);
+
+    let circle_path = path_builder.finish().expect("Error: unable to draw circle");
+
+    pixmap.fill_path(
+        &circle_path,
+        &paint,
+        Default::default(),
+        Transform::identity(),
+        None,
+    );
+
+    // paint.set_color(tree_color);
+    //
+    // let mut stroke = Stroke::default();
+    //
+    // stroke.width = 8.0;
+    // stroke.line_cap = LineCap::Round;
+    //
+    // let hex_radius = radius / 2.0;
+    // let mut path_builder = PathBuilder::new();
+    //
+    // path_builder.move_to(
+    //     center_x + hex_radius * (PI / 3.0 * 0.0).sin(),
+    //     center_y + hex_radius * (PI / 3.0 * 0.0).cos(),
+    // );
+    //
+    // path_builder.line_to(
+    //     center_x + hex_radius * (PI / 3.0 * 1.0 as f32).sin(),
+    //     center_y + hex_radius * (PI / 3.0 * 1.0 as f32).cos(),
+    // );
+    //
+    // path_builder.line_to(
+    //     center_x + hex_radius * (PI / 3.0 * 2.0 as f32).sin(),
+    //     center_y + hex_radius * (PI / 3.0 * 2.0 as f32).cos(),
+    // );
+    //
+    // path_builder.line_to(
+    //     center_x + hex_radius * (PI / 3.0 * 3.0 as f32).sin(),
+    //     center_y + hex_radius * (PI / 3.0 * 3.0 as f32).cos(),
+    // );
+    //
+    // path_builder.line_to(
+    //     center_x + hex_radius * (PI / 3.0 * 4.0 as f32).sin(),
+    //     center_y + hex_radius * (PI / 3.0 * 4.0 as f32).cos(),
+    // );
+    //
+    // path_builder.line_to(
+    //     center_x + hex_radius * (PI / 3.0 * 5.0 as f32).sin(),
+    //     center_y + hex_radius * (PI / 3.0 * 5.0 as f32).cos(),
+    // );
+    //
+    // path_builder.line_to(
+    //     center_x + hex_radius * (PI / 3.0 * 6.0 as f32).sin(),
+    //     center_y + hex_radius * (PI / 3.0 * 6.0 as f32).cos(),
+    // );
+    //
+    // let hex_path = path_builder
+    //     .finish()
+    //     .expect("Error: unable to draw hexagon");
+    //
+    // pixmap.stroke_path(&hex_path, &paint, &stroke, Transform::identity(), None);
+
+    pixmap
+        .save_png(path)
+        .expect("Error: unable to save wallpaper");
+
+    Command::new("hyprctl")
+        .args(["hyprpaper", "preload", path])
+        .output()
+        .expect("Error: unable to preload wallpaper");
+
+    Command::new("hyprctl")
+        .args(["hyprpaper", "wallpaper", &format!(",{}", path)])
+        .output()
+        .expect("Error: failed to set wallpaper");
+
+    Command::new("hyprctl")
+        .args(["hyprpaper", "unload", "all"])
+        .output()
+        .expect("Error: failed to unload old wallpapers");
+
+    println!("Done drawing wallpaper");
+}
