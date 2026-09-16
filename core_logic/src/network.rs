d6e598f7e6c156653b5f2d05b82ab6439af8e176
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2026-09-16 10:02:29 +0200

    Calling wttr.in with HTTP instead of HTTPS.

--- core_logic/src/network.rs
+++ core_logic/src/network.rs
@@ -1,7 +1,7 @@
 use reqwest::Result;
 
 pub async fn fetch_wttr_data() -> Result<String> {
-    reqwest::get("https://wttr.in?format=j1")
+    reqwest::get("http://wttr.in?format=j1")
         .await?
         .text()
         .await
